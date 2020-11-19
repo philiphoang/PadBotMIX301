96bff2aca3e921b588f4d6fd6a28fec8fb4c4de1
Philip <philip.t.hoang@outlook.com>
2020-11-19 16:28:09 +0100

    Create Intent.java

---
+++ PadBot-SDK-master/PadBotSDK_Android_3.1.0/demo/PadBotSDKDemo/app/src/main/java/cn/inbot/padbotsdkdemo/Intent.java
@@ -0,0 +1,23 @@
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        sendIntent.setType("text/plain");
+
+        Intent shareIntent = Intent.createChooser(sendIntent, null);
+        startActivity(shareIntent);
+    }
+
+}
